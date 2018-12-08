07ba705978a5bf459f62dfdfeb37c0435863d954
Saerom Park <psr6275@naver.com>
2018-12-08 01:32:43 -0800

    fixing indent error

--- mnist_clf.py
+++ mnist_clf.py
@@ -24,7 +24,7 @@ def cnn_model_explicit(x,train_mode = True):
                     activation= tf.nn.relu)
         pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size = [2,2],
                     strides = 2)
-            conv2 = tf.layers.conv2d(inputs = pool1,
+        conv2 = tf.layers.conv2d(inputs = pool1,
                     filters = 64, kernel_size = [5,5],
                     padding = 'same', activation = tf.nn.relu)
         pool2 = tf.layers.max_pooling2d(inputs = conv2, pool_size = [2,2],
